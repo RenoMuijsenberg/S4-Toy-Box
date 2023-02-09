bbbfc19f7a3ec782d76d37bd69558f76bfc3e915
brettmulderit <mulderit.brett@gmail.com>
2023-02-09 11:17:44 +0100

    Changed default page

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="home" element={<Home/>}/>
+                    <Route path="" element={<Home/>}/>
                     <Route path="map" element={<Map/>}/>
                 </Routes>
             </BrowserRouter>
