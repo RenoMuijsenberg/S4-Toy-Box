6f2fd98191fa2b6cf80d7d10bc062dde9e8432f9
Reno Muijsenberg <490412@student.fontys.nl>
2023-02-09 11:26:24 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="" element={<Home/>}/>
+                    <Route path="/" element={<Home/>}/>
                     <Route path="map" element={<Map/>}/>
                 </Routes>
             </BrowserRouter>
