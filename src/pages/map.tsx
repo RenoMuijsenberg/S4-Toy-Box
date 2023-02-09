859cd6d68afe4bf18bba7f33923dead932d91e83
Reno Muijsenberg <490412@student.fontys.nl>
2023-02-09 10:42:13 +0100

    Fix teddy marker

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -4,6 +4,13 @@ import SearchBar from "../components/SearchBar";
 import {LatLng} from "leaflet";
 import styles from "./styles/map.module.css"
 
+import L from 'leaflet';
+
+const iconTeddy = new L.Icon({
+    iconUrl: require('../assets/teddy.png'),
+    iconSize: new L.Point(60, 75),
+});
+
 const Map = () => {
     const position = new LatLng(51.505, -0.09)
     return (
@@ -14,7 +21,7 @@ const Map = () => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={position} icon={iconTeddy}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
