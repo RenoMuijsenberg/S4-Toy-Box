e3be1eebc9c2460293d42f556c560bdf892901ec
Reno Muijsenberg <490412@student.fontys.nl>
2023-02-09 11:39:43 +0100

    Restyle list item

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,13 @@
 import SearchBar from "../components/SearchBar";
 import Navbar from "../components/navbar";
+import ToyListView from "../components/ToyListView";
 
 const Home = () => {
     return (
         <div>
             <SearchBar/>
+            <ToyListView />
             <Navbar/>
-
         </div>
     );
 };
