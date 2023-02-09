6547c34c9b123dc86e0d51076c7dc773d9517f1f
brettmulderit <mulderit.brett@gmail.com>
2023-02-09 10:54:47 +0100

    Added more teddybears

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -6,7 +6,15 @@ import styles from "./styles/map.module.css"
 
 import L from 'leaflet';
 
-const iconTeddy = new L.Icon({
+const iconTeddySmall = new L.Icon({
+    iconUrl: require('../assets/teddy.png'),
+    iconSize: new L.Point(20, 25),
+});
+const iconTeddyMedium = new L.Icon({
+    iconUrl: require('../assets/teddy.png'),
+    iconSize: new L.Point(30, 37.5),
+});
+const iconTeddyBig = new L.Icon({
     iconUrl: require('../assets/teddy.png'),
     iconSize: new L.Point(60, 75),
 });
@@ -16,15 +24,16 @@ const Map = () => {
     return (
         <div className={styles.mapPage}>
             <SearchBar/>
-            <MapContainer center={position} className={styles.map} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={new LatLng(51.444004, 5.456020)} className={styles.map} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position} icon={iconTeddy}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
+                <Marker position={new LatLng(51.444004, 5.456020)} icon={iconTeddySmall}>
+                </Marker>
+                <Marker position={new LatLng(51.448859, 5.474573)} icon={iconTeddyMedium}>
+                </Marker>
+                <Marker position={new LatLng(51.435032, 5.508620)} icon={iconTeddyBig}>
                 </Marker>
             </MapContainer>,
             <Navbar/>
