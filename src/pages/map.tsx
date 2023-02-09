64559a7069ddfd1bc675c261777849f5eca1a883
brettmulderit <mulderit.brett@gmail.com>
2023-02-09 10:32:49 +0100

    Added map

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,7 +1,26 @@
+import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
+import Navbar from "../components/navbar";
+import SearchBar from "../components/SearchBar";
+import {LatLng} from "leaflet";
+import styles from "./styles/map.module.css"
+
 const Map = () => {
+    const position = new LatLng(51.505, -0.09)
     return (
-        <div>
-            Hello
+        <div className={styles.mapPage}>
+            <SearchBar/>
+            <MapContainer center={position} className={styles.map} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>,
+            <Navbar/>
         </div>
     );
 };
